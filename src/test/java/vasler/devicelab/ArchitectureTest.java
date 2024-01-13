56cd3c96f335f2763447fa75ac988ab098a2e211
Valentin Ašler <valentin.asler@gmail.com>
2024-01-13 13:20:24 +0100

    Added ArchUnit tests for DDD and Ports&Adapters architecture

--- src/test/java/vasler/devicelab/ArchitectureTest.java
+++ src/test/java/vasler/devicelab/ArchitectureTest.java
@@ -1,15 +1,19 @@
-//package vasler.devicelab;
-//
-//import com.tngtech.archunit.junit.AnalyzeClasses;
-//import com.tngtech.archunit.junit.ArchTest;
-//import com.tngtech.archunit.lang.ArchRule;
-//import org.jmolecules.archunit.JMoleculesArchitectureRules;
-//import org.jmolecules.archunit.JMoleculesDddRules;
-//
-//@AnalyzeClasses(packagesOf = DevicelabApplication.class)
-//public class ArchitectureTest {
+package vasler.devicelab;
+
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+import org.jmolecules.archunit.JMoleculesArchitectureRules;
+import org.jmolecules.archunit.JMoleculesDddRules;
+
+@AnalyzeClasses(packagesOf = Application.class)
+public class ArchitectureTest {
+
 //    @ArchTest
-//    public ArchRule ddd = JMoleculesDddRules.all();
-//    @ArchTest
-//    public ArchRule hexagonal = JMoleculesArchitectureRules.ensureHexagonal();
-//}
+//    public static final ArchRule myRule = classes().that().resideInAPackage("..service..").should().onlyBeAccessed().byAnyPackage("..controller..");
+
+    @ArchTest
+    public static final ArchRule ddd = JMoleculesDddRules.all();
+    @ArchTest
+    public static final ArchRule hexagonal = JMoleculesArchitectureRules.ensureHexagonal();
+}
